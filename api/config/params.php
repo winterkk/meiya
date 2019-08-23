24ffc1dab6ea8a3d395ec20b721a1240e765cf33
zachary <zachary@email.com>
2019-08-23 17:37:31 +0800

    mod pic path

--- api/config/params.php
+++ api/config/params.php
@@ -3,7 +3,4 @@ return [
     'adminEmail' => 'admin@example.com',
     // token 
     'tokenSecret' => '75b8bb5b854ecb616947cd4281cc2cba780fcf90',
-
-    // upload path
-    'uploadPath' => __DIR__ . '/../../uploads'
 ];
--- common/config/params.php
+++ common/config/params.php
@@ -3,4 +3,11 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+
+    /* 上传文件 */
+    'upload' => [
+        //'path' => Yii::getAlias('@base/web/storage/image/'), // 服务器解析到/web/目录时，上传到这里
+        'image' => Yii::getAlias('@storage/web/images/'),
+        'file' => Yii::getAlias('@storage/web/files'),
+    ],
 ];
